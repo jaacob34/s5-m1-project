e509ee6d4bb48e43297181f5d648e7cf8b72f05a
jaacob34 <jacobhernandez34@icloud.com>
2024-07-30 00:30:49 -0700

    completed tasks 3, 4, and 5

--- frontend/index.js
+++ frontend/index.js
@@ -12,14 +12,10 @@ function moduleProject1() {
 
   // 👉 TASK 2 - Build a "Quote of the Day" widget
   //  ✨ add your code here
-  console.log(quotes)
   const quoteOfTheDay = document.querySelector('.quoteoftheday')
-  const div = document.createElement('div')
-
-
-  console.log(quoteOfTheDay)
+  let num = Math.floor(Math.random() * 10)
+  
   let addQuotes = () => {
-    let num = Math.floor(Math.random() * 10)
     const quote = document.createElement('div')
     const authorAndDate = document.createElement('div')
     quote.textContent = quotes[num].quote
@@ -28,14 +24,78 @@ function moduleProject1() {
     quoteOfTheDay.append(authorAndDate)
   }
   addQuotes()
+
   // 👉 TASK 3 - Build a "Corporate Speak" widget
   //  ✨ add your code here
+  const corporateSpeak = document.querySelector('.corporatespeak')
+  const speech = document.createElement('p')
+  corporateSpeak.append(speech)
+  
+  const verb1 = verbs[Math.floor(Math.random() * 10)]
+  const verb2 = verbs[Math.floor(Math.random() * 10)]
+  const noun1 = nouns[Math.floor(Math.random() * 10)]
+  const noun2 = nouns[Math.floor(Math.random() * 10)]
+  const adverb1 = adverbs[Math.floor(Math.random() * 10)]
+  const adverb2 = adverbs[Math.floor(Math.random() * 10)]
+
+  speech.textContent = `We need to ${verb1} our ${noun1} ${adverb1} in order to ${verb2} our ${noun2} ${adverb2}.`
+  
+  
+
+  
 
   // 👉 TASK 4 - Build a "Countdown" widget
   //  ✨ add your code here
+  const countdown = document.querySelector('.countdown')
+  const countP = document.createElement('p')
+  countdown.append(countP)
+  const countdownArr = ['Liftoff! 🚀', 'T-minus 1...', 'T-minus 2...', 'T-minus 3...', 'T-minus 4...', 'T-minus 5...']
+
+  let count = countdownArr.length - 1
+  
+  function countLoop() {
+    countP.textContent = countdownArr[count]
+    count--
+    if (count >= 0) {
+      setTimeout(countLoop, 1000)
+    }
+  }
+  countLoop()
 
   // 👉 TASK 5 - Build a "Friends" widget
   //  ✨ add your code here
+  const friendWidget = document.querySelector('.friends')
+  const friendP = document.createElement('p')
+  friendWidget.append(friendP)
+  console.log(friendWidget)
+  const randomPerson = people[Math.floor(Math.random() * people.length)]
+  const birthYear = randomPerson.dateOfBirth.substring(0, 4)
+  const friendIds = randomPerson.friends
+
+  const randomFriends = people
+  .filter(person => friendIds.includes(person.id))
+  .map(person => `${person.fname} ${person.lname}`)
+
+  console.log(randomPerson)
+  console.log(randomFriends)
+  console.log(birthYear)
+
+  friendP.textContent = `${randomPerson.fname} ${randomPerson.lname} was born in ${birthYear}`
+  if (!randomFriends.length) {
+    friendP.textContent += ' and has no friends.'
+  } else {
+    for (let i = randomFriends.length; i >= 0; i--) {
+
+      if (i === randomFriends.length - 1) {
+        friendP.textContent += ` and is friends with ${randomFriends[i]}`
+      } else if (i === 0) {
+        friendP.textContent += ` and ${randomFriends[i]}.`
+      } else if (i < randomFriends.length - 1) {
+        friendP.textContent += `, ${randomFriends[i]}`
+      }
+    }
+  }
+
 
   // 👉 TASK 6 - Make it so user can tab through the widgets
   //  ✨ add your code here
